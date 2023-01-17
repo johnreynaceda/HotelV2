96709d5fc70cbd3244dc79a266eb38eb797c040a
GabJeungSook <67619600+GabJeungSook@users.noreply.github.com>
2023-01-18 07:47:24 +0800

    s

--- resources/views/livewire/frontdesk/monitoring/room-monitoring.blade.php
+++ resources/views/livewire/frontdesk/monitoring/room-monitoring.blade.php
@@ -46,7 +46,7 @@
               class="whitespace-nowrap py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">Room
               Number</th>
             <th scope="col" class="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900">Floor
-            </th>
+            </th> 
             <th scope="col" class="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900">
               Status</th>
             <th scope="col" class="relative whitespace-nowrap py-3.5 pl-3 pr-4 sm:pr-6">
