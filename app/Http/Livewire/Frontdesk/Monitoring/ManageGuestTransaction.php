9bc43092ae0777553a91cf815debdff639584218
GabJeungSook <67619600+GabJeungSook@users.noreply.github.com>
2023-01-18 23:05:44 +0800

    amenities

--- app/Http/Livewire/Frontdesk/Monitoring/ManageGuestTransaction.php
+++ app/Http/Livewire/Frontdesk/Monitoring/ManageGuestTransaction.php
@@ -3,15 +3,26 @@
 namespace App\Http\Livewire\Frontdesk\Monitoring;
 
 use Livewire\Component;
+use DB;
 use App\Models\Guest;
 use App\Models\Transaction;
+use App\Models\CheckinDetail;
+use App\Models\HotelItems;
+use WireUi\Traits\Actions;
 
 class ManageGuestTransaction extends Component
 {
+    use Actions;
     public $guest;
     public $transaction;
     public $transaction_description;
-
+    public $items;
+    public $item_id;
+    public $item_quantity;
+    public $item_price;
+    public $subtotal;
+    public $additional_amount;
+    public $total_amount;
     //modals
     public $transfer_modal = false;
     public $deposit_modal = false;
@@ -19,6 +30,8 @@ class ManageGuestTransaction extends Component
     public $damage_modal = false;
     public $amenities_modal = false;
     public $food_beverages_modal = false;
+
+
 
     public function mount()
     {
@@ -31,13 +44,119 @@ class ManageGuestTransaction extends Component
         )
             ->where('guest_id', request()->id)
             ->get();
+        $this->items = HotelItems::where('branch_id', auth()->user()->branch_id)->get();
+
         $count = $this->transaction->where('description', 'Deposit')->count();
+        $this->item_quantity = 1;
+        $this->item_price = 0;
+        $this->subtotal = 0;
+        $this->additional_amount = 0;
+        $this->total_amount = 0;
+    }
+
+    public function updated($item)
+    {
+        if($item == 'item_id')
+        {
+            if($this->item_quantity != '' && $this->additional_amount != '')
+            {
+                $this->item_price = HotelItems::where('branch_id', auth()->user()->branch_id)->where('id', $this->item_id)->first()->price;
+                $this->subtotal = $this->item_price * $this->item_quantity;
+                $this->total_amount = $this->subtotal + $this->additional_amount;
+            }
+        }
+        
+        if($item == 'item_quantity')
+        {
+            if($this->item_id != null)
+            {
+                if($this->item_quantity == '')
+                {
+                    $this->item_price = HotelItems::where('branch_id', auth()->user()->branch_id)->where('id', $this->item_id)->first()->price;
+                    $this->subtotal = $this->item_price * 1;
+                    $this->total_amount = $this->subtotal + $this->additional_amount;
+                }else if($this->additional_amount == ''){
+                    $this->item_price = HotelItems::where('branch_id', auth()->user()->branch_id)->where('id', $this->item_id)->first()->price;
+                    $this->subtotal = $this->item_price * $this->item_quantity;
+                    $this->total_amount = $this->subtotal + 0;
+                }else if($this->additional_amount == '' && $this->item_quantity == ''){
+                    $this->item_price = HotelItems::where('branch_id', auth()->user()->branch_id)->where('id', $this->item_id)->first()->price;
+                    $this->subtotal = $this->item_price * 1;
+                    $this->total_amount = $this->subtotal + 0;
+                }
+                else{
+                    $this->item_price = HotelItems::where('branch_id', auth()->user()->branch_id)->where('id', $this->item_id)->first()->price;
+                    $this->subtotal = $this->item_price * $this->item_quantity;
+                    $this->total_amount = $this->subtotal + $this->additional_amount;
+                }
+            }
+        }
+
+        if($item == 'additional_amount')
+        {
+            if($this->item_id != null)
+            {
+                if($this->item_quantity == '')
+                {
+                    $this->item_price = HotelItems::where('branch_id', auth()->user()->branch_id)->where('id', $this->item_id)->first()->price;
+                    $this->subtotal = $this->item_price * 1;
+                    $this->total_amount = $this->subtotal + $this->additional_amount;
+                }else if($this->additional_amount == ''){
+                    $this->item_price = HotelItems::where('branch_id', auth()->user()->branch_id)->where('id', $this->item_id)->first()->price;
+                    $this->subtotal = $this->item_price * $this->item_quantity;
+                    $this->total_amount = $this->subtotal + 0;
+                }else if($this->additional_amount == '' && $this->item_quantity == ''){
+                    $this->item_price = HotelItems::where('branch_id', auth()->user()->branch_id)->where('id', $this->item_id)->first()->price;
+                    $this->subtotal = $this->item_price * 1;
+                    $this->total_amount = $this->subtotal + 0;
+                }
+                else{
+                    $this->item_price = HotelItems::where('branch_id', auth()->user()->branch_id)->where('id', $this->item_id)->first()->price;
+                    $this->subtotal = $this->item_price * $this->item_quantity;
+                    $this->total_amount = $this->subtotal + $this->additional_amount;
+                }
+            }
+        }
+    }
+
+    public function addAmenities()
+    {
+        $this->validate([
+            'item_id' => 'required',
+            'item_quantity' => 'required',
+        ]);
+        DB::beginTransaction();
+        $check_in_detail = CheckInDetail::where('guest_id', $this->guest->id)->first();
+        $amenities = HotelItems::where('branch_id', auth()->user()->branch_id)->where('id', $this->item_id)->first();
+
+        Transaction::create([
+            'branch_id' => $check_in_detail->guest->branch_id,
+            'room_id' => $check_in_detail->room_id,
+            'guest_id' => $check_in_detail->guest_id,
+            'floor_id' => $check_in_detail->room->floor_id,
+            'transaction_type_id' => 8,
+            'description' => 'Amenities',
+            'payable_amount' => $this->total_amount,
+            'paid_amount' => 0,
+            'change_amount' => 0,
+            'deposit_amount' => 0,
+            'paid_at' => null,
+            'override_at' => null,
+            'remarks' => 'Guest Added Amenities: ('.$this->item_quantity.')'.' '.$amenities->name,
+        ]);
+        DB::commit();
+        $this->amenities_modal = false;
+        $this->dialog()->success(
+            $title = 'Success',
+            $description = 'Data successfully saved'
+        );    
     }
 
     public function render()
     {
         return view('livewire.frontdesk.monitoring.manage-guest-transaction', [
             'transactions' => $this->transaction,
+            'items' => $this->items,
         ]);
     }
 }
--- resources/views/livewire/frontdesk/monitoring/manage-guest-transaction.blade.php
+++ resources/views/livewire/frontdesk/monitoring/manage-guest-transaction.blade.php
@@ -20,7 +20,10 @@
                 <path d="M5 20h14v2H5v-2zm7-2a8 8 0 1 1 0-16 8 8 0 0 1 0 16z" />
               </svg>
               <div class="ml-2">
+                <div class="flex space-x-1">
                 <p class="text-sm font-medium text-gray-900 uppercase">{{ $guest->name }}</p>
+                <p class="text-sm text-gray-900 ">(09{{ $guest->contact }})</p>
+                </div>
                 <div class="flex space-x-1 items-center fill-gray-600 text-gray-600">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="h-5 w-5">
                     <path fill="none" d="M0 0h24v24H0z" />
@@ -395,14 +398,41 @@
           <x-button.circle icon="plus" xs positive />
         </div>
         <div class="mt-3">
-          Content here
+            <div class="space-y-4">
+            <x-native-select label="Item" wire:model="item_id">
+                <option>Select Item</option>
+                @forelse($items as $item)
+                <option value="{{$item->id}}">{{$item->name}}</option>
+                @empty
+                <option>No Items Yet</option>
+                @endforelse
+            </x-native-select>
+            <x-input label="Quantity" type="number" min="1" value="1" placeholder="" wire:model="item_quantity" />
+            <x-input label="Additional Amount" type="number" min="0" placeholder="" wire:model="additional_amount" />
+            
+            <dl class="mt-8 bg-gray-300 rounded-md p-2 divide-y divide-gray-400 text-sm lg:col-span-5 lg:mt-0">
+              <div class="flex items-center justify-between pb-4">
+                <dt class="text-gray-600">Subtotal</dt>
+                <dd class="font-medium text-gray-800">₱ {{number_format($subtotal, 2, '.', ',');}}</dd>
+              </div>
+              <div class="flex items-center justify-between py-4">
+                <dt class="text-gray-600">Additional Amount</dt>
+                <dd class="font-medium text-gray-800">₱ {{$additional_amount == '' ? '0.00' : number_format($additional_amount, 2, '.', ',');}}</dd>
+              </div>
+              <div class="flex items-center justify-between pt-4">
+                <dt class="font-medium text-lg text-gray-800">Total Payable Amount</dt>
+                <dd class="font-medium text-lg text-gray-900">₱ {{number_format($total_amount, 2, '.', ',');}}</dd>
+              </div>
+            </dl>
+             
+            </div>
         </div>
       </div>
 
       <x-slot name="footer">
         <div class="flex justify-end gap-x-2">
           <x-button flat negative label="Cancel" x-on:click="close" />
-          <x-button positive label="Save" right-icon="arrow-narrow-right" />
+          <x-button positive label="Save" wire:click="addAmenities" right-icon="arrow-narrow-right" />
         </div>
       </x-slot>
     </x-card>
