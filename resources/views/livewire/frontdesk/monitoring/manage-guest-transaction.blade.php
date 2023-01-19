813815f6e89ec37470b97c7ae0b2d8bde821b49c
johnreynaceda <johnreynaceda@sksu.edu.ph>
2023-01-19 15:25:34 +0800

    Update manage-guest-transaction.blade.php

--- resources/views/livewire/frontdesk/monitoring/manage-guest-transaction.blade.php
+++ resources/views/livewire/frontdesk/monitoring/manage-guest-transaction.blade.php
@@ -82,7 +82,7 @@
       <div class="border rounded-lg p-4">
         <div class="lg:flex lg:items-center lg:justify-between border-b">
           <div class="min-w-0 flex-1">
-            <h2 class="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">GUEST
+            <h2 class="text-2xl font-bold leading-7 text-gray-900 s sm:truncate sm:text-3xl sm:tracking-tight">GUEST
               TRANSACTIONS</h2>
             <div class="my-1 flex flex-col sm:mt-0 sm:flex-row sm:flex-wrap sm:space-x-6">
               <div class="mt-2 flex items-center text-sm text-gray-500">
