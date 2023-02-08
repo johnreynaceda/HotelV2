c3f705433b4456020b2cb0716db5d84eca3b568d
GabJeungSook <67619600+GabJeungSook@users.noreply.github.com>
2023-02-08 20:50:18 +0800

    shift logs table

---
+++ database/migrations/2023_02_08_204445_create_shift_logs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shift_logs', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('time_in');
+            $table->dateTime('time_out')->nullable();
+            $table->json('frontdesk_ids');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shift_logs');
+    }
+};
