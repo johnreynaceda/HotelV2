4a063f3dadda0c0184d8293e3a341a0ef2a9f9cd
johnreynaceda <johnreynaceda@sksu.edu.ph>
2023-02-13 21:35:03 +0800

    frontdesk-new-design

--- resources/views/frontdesk/index.blade.php
+++ resources/views/frontdesk/index.blade.php
@@ -1,5 +1,39 @@
 <x-frontdesk-layout>
   <div>
-    sdsds
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis illo voluptates assumenda nisi soluta maxime
+      odit similique maiores sequi dolor aliquam totam, nihil nobis incidunt voluptatibus dignissimos. Ullam,
+      voluptatum. Quos ut doloribus consequuntur est voluptate dolore minima quidem, nisi asperiores harum id, sint
+      modi? Natus optio ipsa, unde quia itaque consectetur iste rerum nihil voluptas iure facilis similique, laudantium
+      deserunt dolores eum vitae provident ipsam reiciendis modi nesciunt eos autem delectus, veniam aliquid. Ab numquam
+      pariatur doloremque provident eligendi. Mollitia et tenetur totam in minima ex deleniti ratione, alias hic dolore,
+      possimus tempora. Voluptates nisi, veritatis optio consectetur incidunt quaerat odio fugiat magnam labore! Unde
+      omnis neque blanditiis reprehenderit. Atque at, eum, fugiat dolorum natus in eaque ratione corporis enim a
+      cupiditate perferendis necessitatibus doloribus officiis repellat qui praesentium blanditiis quam cumque quaerat
+      delectus. Consequuntur, cupiditate suscipit vero odio rem autem magni accusantium ad quas earum officiis quis? Eos
+      mollitia, voluptas possimus eum officiis cumque dolor alias earum sequi debitis, ullam deserunt provident quidem
+      commodi nam minima ea quaerat. Ea dolores hic fuga odio corrupti sapiente reprehenderit odit, aut expedita porro
+      eos officia voluptatibus accusantium sed facilis totam sint tempore soluta quisquam tempora maiores? Delectus
+      officia eum iusto eveniet a, accusamus, inventore quibusdam illo, fuga iure harum nostrum ex et eos omnis
+      reprehenderit beatae. Odit perferendis tenetur velit amet molestias. Atque, sed facilis asperiores illum natus
+      placeat sit consequatur necessitatibus quibusdam, porro ullam fugiat! Laudantium cumque similique, autem
+      reiciendis at debitis suscipit rem, qui beatae aspernatur optio quia quam aliquid sint quisquam vero nemo,
+      provident praesentium numquam enim quidem. Consequuntur dolores necessitatibus facilis nulla autem aut ex
+      reiciendis maxime quae maiores, optio soluta earum commodi sint quo deserunt repudiandae odit molestias? Maiores
+      harum ea ad nisi dolor voluptates, maxime corporis molestiae dolore, officia alias dolorum nam consectetur tempora
+      non ipsum sapiente saepe veritatis adipisci in cupiditate velit? Eos, nulla suscipit sunt veritatis sapiente
+      voluptatum, laboriosam perspiciatis quidem iure delectus ad quo eius ducimus architecto? Non harum commodi nihil,
+      reprehenderit vitae dignissimos nulla sint cupiditate. Quasi, blanditiis. Atque sapiente, facere facilis
+      repudiandae deserunt quae delectus itaque aliquam commodi eos error veniam quas nemo, eligendi aspernatur odio
+      dolorem repellendus cumque suscipit dignissimos impedit? Aliquid, vel! Vero harum odio labore quas eius veritatis
+      sit alias ea facere optio quasi minus vel explicabo corrupti obcaecati consectetur itaque, suscipit ullam!
+      Adipisci porro natus vero maiores itaque nostrum doloremque voluptatibus, eligendi incidunt quae corporis
+      temporibus optio eius soluta repudiandae enim! Tempore autem quasi molestias ducimus suscipit illo quas aut sequi
+      modi sint amet ipsum aliquid in officiis ea, cupiditate dolores sapiente maxime dolore assumenda nulla enim.
+      Quibusdam, aspernatur necessitatibus. Nulla quod maiores magnam, excepturi necessitatibus, earum, ratione labore
+      impedit ullam qui facilis aspernatur laudantium! Eum, recusandae minus amet dolor enim accusamus accusantium ab
+      rerum alias voluptas, exercitationem dignissimos. Quo aut doloremque facilis libero vitae repellendus corporis
+      molestias accusamus architecto sunt quaerat ad maxime itaque veniam assumenda voluptatibus consequuntur repellat,
+      saepe voluptatem ex. Soluta, consequuntur cumque dolor autem deserunt accusantium voluptas pariatur vitae laborum,
+      voluptate praesentium voluptatibus.</p>
   </div>
 </x-frontdesk-layout>
