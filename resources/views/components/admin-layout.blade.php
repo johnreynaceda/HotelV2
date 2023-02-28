d35b5057b1c1117c6ead650778c49898592ad836
johnreynaceda <johnreynaceda@sksu.edu.ph>
2023-02-28 09:30:57 +0800

    Update admin-layout.blade.php

--- resources/views/components/admin-layout.blade.php
+++ resources/views/components/admin-layout.blade.php
@@ -932,16 +932,6 @@
 
   <div x-show="logout" x-cloak class="relative z-10" aria-labelledby="modal-title" role="dialog"
     aria-modal="true">
-    <!--
-    Background backdrop, show/hide based on modal state.
-
-    Entering: "ease-out duration-300"
-      From: "opacity-0"
-      To: "opacity-100"
-    Leaving: "ease-in duration-200"
-      From: "opacity-100"
-      To: "opacity-0"
-  -->
     <div x-show="logout" x-cloak x-transition:enter="ease-out duration-300" x-transition:enter-start="opacity-0"
       x-transition:enter-end="opacity-100" x-transition:leave="ease-in duration-200"
       x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0"
@@ -950,16 +940,7 @@
 
     <div class="fixed inset-0 z-10 overflow-y-auto">
       <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-        <!--
-        Modal panel, show/hide based on modal state.
 
-        Entering: "ease-out duration-300"
-          From: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-          To: "opacity-100 translate-y-0 sm:scale-100"
-        Leaving: "ease-in duration-200"
-          From: "opacity-100 translate-y-0 sm:scale-100"
-          To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-      -->
         <div x-show="logout" x-cloak x-transition:enter="ease-out duration-300"
           x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100" x-transition:leave="ease-in duration-200"
