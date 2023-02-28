d3cb6bdaa33e63e825f9e0803e1935e1371df48d
johnreynaceda <johnreynaceda@sksu.edu.ph>
2023-02-28 11:35:24 +0800

    Update back-office-layout.blade.php

--- resources/views/components/back-office-layout.blade.php
+++ resources/views/components/back-office-layout.blade.php
@@ -14,6 +14,8 @@
   <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;600;700;800;900&display=swap"
     rel="stylesheet">
 
+  <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet">
+
   <style>
 
   </style>
