9ca0fb1f6b114dba70a47c68306d475cfe5eddfe
GabJeungSook <gicawalo@gmail.com>
2023-02-28 11:34:08 +0800

    added new guest report

---
+++ database/migrations/2023_02_28_110640_create_new_guest_reports_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('new_guest_reports', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('checkin_details_id');
+            $table->string('shift_date');
+            $table->string('shift');
+            $table->integer('frontdesk_id');
+            $table->string('partner_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('new_guest_reports');
+    }
+};
