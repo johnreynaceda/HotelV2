d7342ebfe9e5f6115ae2d0ff98bb92af13223709
Gabriel Jon Icawalo <67619600+GabJeungSook@users.noreply.github.com>
2024-03-26 16:46:01 +0800

    animate fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -134,7 +134,7 @@
                     <img
                       src="https://images.unsplash.com/photo-1541971875076-8f970d573be6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"
                       src="" alt=""
-                      class="aspect-[2/3] animate__animated animate__fadeIn  w-full rounded-xl bg-gray-900/5 object-cover shadow-lg">
+                      class="aspect-[2/3] animate__animated animate__fadeInUp  w-full rounded-xl bg-gray-900/5 object-cover shadow-lg">
                     <div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10">
                     </div>
                   </div>
