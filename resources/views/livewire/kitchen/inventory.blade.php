3c3dab7aa4dce8f47db910ae003a480edafc5695
Gabriel Jon Icawalo <67619600+GabJeungSook@users.noreply.github.com>
2024-04-03 15:52:11 +0800

    s

--- resources/views/livewire/kitchen/inventory.blade.php
+++ resources/views/livewire/kitchen/inventory.blade.php
@@ -15,7 +15,7 @@
                           <tr>
                             <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6 lg:pl-8">Name</th>
                             <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Price</th>
-                            <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Quantity</th>
+                            <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Quantity (Number of Servings)</th>
                             <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6 lg:pr-8">
                               <span class="sr-only">Edit</span>
                             </th>
