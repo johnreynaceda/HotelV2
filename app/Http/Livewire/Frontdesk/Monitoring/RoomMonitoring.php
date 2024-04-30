ae79170073c2472d7bb43f90f918599c6e7e4658
GabJeungSook <gicawalo@gmail.com>
2024-04-30 20:50:09 +0800

    ss

--- app/Http/Livewire/Frontdesk/Monitoring/RoomMonitoring.php
+++ app/Http/Livewire/Frontdesk/Monitoring/RoomMonitoring.php
@@ -223,7 +223,7 @@ class RoomMonitoring extends Component
             })
             ->with('floor')
             ->with('checkInDetails', function ($query) {
-                $query->orderBy('check_out_at', 'desc');
+                $query->orderBy('check_out_at', 'asc');
             })
             ->orderByRaw('FIELD(status,"Occupied") DESC')
             ->orderBy('number', 'ASC')
