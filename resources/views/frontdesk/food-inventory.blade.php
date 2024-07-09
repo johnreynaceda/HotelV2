88313d95982848dd969d6259a4bd2a3b6faca735
Gabriel Jon Icawalo <67619600+GabJeungSook@users.noreply.github.com>
2024-07-09 11:38:46 +0800

    opacity

--- resources/views/frontdesk/food-inventory.blade.php
+++ resources/views/frontdesk/food-inventory.blade.php
@@ -26,7 +26,7 @@
             <div  class="block">
                 <div class="relative rounded-xl shadow-lg overflow-hidden h-72">
                     <img src="{{ asset('images/category.jpg') }}" alt="Category Image" class="w-full h-full object-cover transition-transform duration-300 transform hover:scale-105">
-                    <div class="absolute inset-0 bg-black bg-opacity-25 transition-opacity duration-300 opacity-100 hover:opacity-100"></div>
+                    <div class="absolute inset-0 bg-black bg-opacity-50 transition-opacity duration-300 opacity-75 hover:opacity-100"></div>
                     <div class="absolute inset-0 flex flex-col items-start justify-between p-6 transition-opacity duration-300 opacity-100 hover:opacity-100">
                         <h1 class="text-3xl font-bold text-white">CATEGORY</h1>
                         <a href="{{ route('frontdesk.food-category') }}" class="shadow-lg px-4 py-1 bg-gray-200 text-gray-600 rounded-full hover:bg-gray-100 transition-colors duration-300"><span class="text-sm">Manage Category</span></a>
@@ -34,14 +34,16 @@
                 </div>
             </div>
         </div>
-        <div class="w-72 p-2 lg:w-72 md:w-72">
-            <div  class="block">
-                <div class="relative rounded-xl shadow-lg overflow-hidden h-72">
+        <div class="w-72 p-2 lg:w-72 md:w-72 cursor-pointer">
+            <div  class="block ">
+                <div class="relative rounded-xl shadow-lg overflow-hidden h-72 ">
                     <img src="{{ asset('images/menu.jpg') }}" alt="Menu Image" class="w-full h-full object-cover transition-transform duration-300 transform hover:scale-105">
-                    <div class="absolute inset-0 bg-black bg-opacity-25 transition-opacity duration-300 opacity-100 hover:opacity-100"></div>
+                    <div class="absolute inset-0 bg-black bg-opacity-50 transition-opacity duration-300 opacity-75 hover:opacity-100"></div>
                     <div class="absolute inset-0 flex flex-col items-start justify-between p-6 transition-opacity duration-300 opacity-100 hover:opacity-100">
                         <h1 class="text-3xl font-bold text-white">MENU</h1>
-                        <a href="{{ route('frontdesk.food-menu') }}" class="shadow-lg px-4 py-1 bg-gray-200 text-gray-600 rounded-full hover:bg-gray-100 transition-colors duration-300"><span class="text-sm">Manage Menu</span></a>
+                        <a href="{{ route('frontdesk.food-menu') }}" class="shadow-lg px-4 py-1 bg-gray-200 text-gray-600 rounded-full hover:bg-gray-100 transition-colors duration-300">
+                                <span class="text-sm">Manage Menu</span>
+                          </a>
                     </div>
                 </div>
             </div>
@@ -50,7 +52,7 @@
             <div  class="block">
                 <div class="relative rounded-xl shadow-lg overflow-hidden h-72">
                     <img src="{{ asset('images/inventories.jpg') }}" alt="Category Image" class="w-full h-full object-cover transition-transform duration-300 transform hover:scale-105 scale-100">
-                    <div class="absolute inset-0 bg-black bg-opacity-25 transition-opacity duration-300 opacity-100 hover:opacity-100"></div>
+                    <div class="absolute inset-0 bg-black bg-opacity-50 transition-opacity duration-300 opacity-75 hover:opacity-100"></div>
                     <div class="absolute inset-0 flex flex-col items-start justify-between p-6 transition-opacity duration-300 opacity-100 hover:opacity-100">
                         <h1 class="text-3xl font-bold text-white">INVENTORY</h1>
                         <a href="{{ route('frontdesk.food-inventories') }}" class="shadow-lg px-4 py-1 bg-gray-200 text-gray-600 rounded-full hover:bg-gray-100 transition-colors duration-300"><span class="text-sm">Manage Inventory</span></a>
