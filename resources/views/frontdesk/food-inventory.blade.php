aaaa0e46c8b28937f62f082925059eba9280581f
Gabriel Jon Icawalo <67619600+GabJeungSook@users.noreply.github.com>
2024-07-09 09:16:28 +0800

    redesign of kitchen cards

--- resources/views/frontdesk/food-inventory.blade.php
+++ resources/views/frontdesk/food-inventory.blade.php
@@ -1,7 +1,7 @@
 <x-admin-layout>
     <div class="container items-center py-8 m-auto mt-5">
         <div class="flex flex-wrap pb-3 mx-2 md:mx-24 lg:mx-0">
-          <div class="w-full p-2 lg:w-1/4 md:w-1/2">
+          {{-- <div class="w-full p-2 lg:w-1/4 md:w-1/2">
             <a href="{{route('frontdesk.food-category')}}">
                 <div
                 class="flex flex-col px-6 py-10 overflow-hidden bg-white hover:bg-gradient-to-br hover:from-purple-400 hover:via-blue-400 hover:to-blue-500 rounded-xl shadow-lg duration-300 hover:shadow-2xl group">
@@ -21,8 +21,44 @@
                 <h1 class="text-3xl sm:text-4xl xl:text-5xl font-bold text-gray-700 mt-12 group-hover:text-gray-50">Category</h1>
               </div>
             </a>
-          </div>
-          <div class="w-full p-2 lg:w-1/4 md:w-1/2">
+          </div> --}}
+          <div class="w-72 p-2 lg:w-72 md:w-72">
+            <div  class="block">
+                <div class="relative rounded-xl shadow-lg overflow-hidden h-72">
+                    <img src="{{ asset('images/category.jpg') }}" alt="Category Image" class="w-full h-full object-cover transition-transform duration-300 transform hover:scale-105">
+                    <div class="absolute inset-0 bg-black bg-opacity-25 transition-opacity duration-300 opacity-100 hover:opacity-100"></div>
+                    <div class="absolute inset-0 flex flex-col items-start justify-between p-6 transition-opacity duration-300 opacity-100 hover:opacity-100">
+                        <h1 class="text-3xl font-bold text-white">CATEGORY</h1>
+                        <a href="{{ route('frontdesk.food-category') }}" class="shadow-lg px-4 py-1 bg-gray-200 text-gray-600 rounded-full hover:bg-gray-100 transition-colors duration-300"><span class="text-sm">View Category</span></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="w-72 p-2 lg:w-72 md:w-72">
+            <div  class="block">
+                <div class="relative rounded-xl shadow-lg overflow-hidden h-72">
+                    <img src="{{ asset('images/menu.jpg') }}" alt="Menu Image" class="w-full h-full object-cover transition-transform duration-300 transform hover:scale-105">
+                    <div class="absolute inset-0 bg-black bg-opacity-25 transition-opacity duration-300 opacity-100 hover:opacity-100"></div>
+                    <div class="absolute inset-0 flex flex-col items-start justify-between p-6 transition-opacity duration-300 opacity-100 hover:opacity-100">
+                        <h1 class="text-3xl font-bold text-white">MENU</h1>
+                        <a href="{{ route('frontdesk.food-menu') }}" class="shadow-lg px-4 py-1 bg-gray-200 text-gray-600 rounded-full hover:bg-gray-100 transition-colors duration-300"><span class="text-sm">View Menu</span></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="w-72 p-2 lg:w-72 md:w-72">
+            <div  class="block">
+                <div class="relative rounded-xl shadow-lg overflow-hidden h-72">
+                    <img src="{{ asset('images/inventories.jpg') }}" alt="Category Image" class="w-full h-full object-cover transition-transform duration-300 transform hover:scale-105 scale-100">
+                    <div class="absolute inset-0 bg-black bg-opacity-25 transition-opacity duration-300 opacity-100 hover:opacity-100"></div>
+                    <div class="absolute inset-0 flex flex-col items-start justify-between p-6 transition-opacity duration-300 opacity-100 hover:opacity-100">
+                        <h1 class="text-3xl font-bold text-white">INVENTORY</h1>
+                        <a href="{{ route('frontdesk.food-inventories') }}" class="shadow-lg px-4 py-1 bg-gray-200 text-gray-600 rounded-full hover:bg-gray-100 transition-colors duration-300"><span class="text-sm">View Inventory</span></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+          {{-- <div class="w-full p-2 lg:w-1/4 md:w-1/2">
             <a href="{{route('frontdesk.food-menu')}}">
                 <div
                 class="flex flex-col px-6 py-10 overflow-hidden bg-white hover:bg-gradient-to-br hover:from-purple-400 hover:via-blue-400 hover:to-blue-500 rounded-xl shadow-lg duration-300 hover:shadow-2xl group">
@@ -56,7 +92,7 @@
                 <h1 class="text-3xl sm:text-4xl xl:text-5xl font-bold text-gray-700 mt-12 group-hover:text-gray-50">Inventory</h1>
               </div>
             </a>
-          </div>
+          </div> --}}
         </div>
       </div>
 
