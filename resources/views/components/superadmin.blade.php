9c09b93bc492f30ff2bd212a831b2c91c3e5ca3c
Gabriel Jon Icawalo <67619600+GabJeungSook@users.noreply.github.com>
2025-04-07 08:42:03 +0800

    logout

--- resources/views/components/superadmin.blade.php
+++ resources/views/components/superadmin.blade.php
@@ -132,10 +132,10 @@
                             <button href="{{ route('logout') }}"
                               onclick="event.preventDefault();
                                 this.closest('form').submit();"
-                              class="bg-gradient-to-r from-gray-500 via-gray-500 to-transparent p-2 px-4 flex space-x-1 rounded-full">
+                              class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1">
 
                               <span class="font-semibold text-gray-100 uppercase">Logout</span>
-                              <svg class="w-6 h-6 text-gray-100" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+                              {{-- <svg class="w-6 h-6 text-gray-100" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
                                 fill="currentColor" aria-hidden="true">
                                 <path
                                   d="M15.24 22.27h-.13c-4.44 0-6.58-1.75-6.95-5.67-.04-.41.26-.78.68-.82.4-.04.78.27.82.68.29 3.14 1.77 4.31 5.46 4.31h.13c4.07 0 5.51-1.44 5.51-5.51V8.74c0-4.07-1.44-5.51-5.51-5.51h-.13c-3.71 0-5.19 1.19-5.46 4.39-.05.41-.4.72-.82.68a.751.751 0 01-.69-.81c.34-3.98 2.49-5.76 6.96-5.76h.13c4.91 0 7.01 2.1 7.01 7.01v6.52c0 4.91-2.1 7.01-7.01 7.01z">
@@ -145,7 +145,7 @@
                                 <path
                                   d="M5.85 16.1c-.19 0-.38-.07-.53-.22l-3.35-3.35a.754.754 0 010-1.06l3.35-3.35c.29-.29.77-.29 1.06 0 .29.29.29.77 0 1.06L3.56 12l2.82 2.82c.29.29.29.77 0 1.06-.14.15-.34.22-.53.22z">
                                 </path>
-                              </svg>
+                              </svg> --}}
                             </button>
                           </form>
                       {{-- <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1"
