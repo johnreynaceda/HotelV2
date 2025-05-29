cd3a3901309ffdbd6932439bf379e19f25aecb2a
GabJeungSook <67619600+GabJeungSook@users.noreply.github.com>
2025-05-29 11:51:29 +0800

    try

--- resources/views/components/kiosk-layout.blade.php
+++ resources/views/components/kiosk-layout.blade.php
@@ -7,6 +7,7 @@
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
   <title>{{ config('app.name', 'Laravel') }}</title>
+  <link rel="manifest" href="/manifest.webmanifest">
 
   <!-- Fonts -->
   <link rel="preconnect" href="https://fonts.googleapis.com">
