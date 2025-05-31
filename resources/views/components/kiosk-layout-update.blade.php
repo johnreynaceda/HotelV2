2eb4c4115759a6caa4bf8949c8005f0a0f4ec0d1
GabJeungSook <67619600+GabJeungSook@users.noreply.github.com>
2025-05-31 15:02:52 +0800

    rev

--- resources/views/components/kiosk-layout-update.blade.php
+++ resources/views/components/kiosk-layout-update.blade.php
@@ -82,35 +82,41 @@ aria-modal="true">
 <div class="fixed inset-0 z-50 overflow-y-auto">
   <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
 
-    <div id="logoutModal" style="display: none;"
+    <div x-show="logout" x-cloak x-transition:enter="ease-out duration-300"
+      x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+      x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100" x-transition:leave="ease-in duration-200"
+      x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
+      x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
       class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm">
       <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-      <div class="sm:flex sm:items-start">
-        <div
-        class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-        <!-- Heroicon name: outline/exclamation-triangle -->
-        <svg class="h-6 w-6 text-red-600" xmlns="http://www.w3.org/2000/svg" fill="none"
-          viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-          <path stroke-linecap="round" stroke-linejoin="round"
-          d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
-        </svg>
+        <div class="sm:flex sm:items-start">
+          <div
+            class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+            <!-- Heroicon name: outline/exclamation-triangle -->
+            <svg class="h-6 w-6 text-red-600" xmlns="http://www.w3.org/2000/svg" fill="none"
+              viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+              <path stroke-linecap="round" stroke-linejoin="round"
+                d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
+            </svg>
+          </div>
+          <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+            <h3 class="text-lg font-medium leading-6 text-gray-900" id="modal-title">Logout Account</h3>
+            <div class="mt-2">
+              <p class="text-sm text-gray-500">Are you sure you want to logout your account? </p>
+            </div>
+          </div>
         </div>
-        <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-        <h3 class="text-lg font-medium leading-6 text-gray-900" id="modal-title">Logout Account</h3>
-        <div class="mt-2">
-          <p class="text-sm text-gray-500">Are you sure you want to logout your account? </p>
-        </div>
-        </div>
-      </div>
       </div>
       <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse  sm:px-6">
-      <form method="POST" action="{{ route('logout') }}" class="flex space-x-2">
-        @csrf
-        <x-button type="button" id="cancelLogoutBtn" label="Cancel" sm icon="x" />
-        <x-button href="{{ route('logout') }}"
-        onclick="event.preventDefault(); this.closest('form').submit();" label="Logout"
-        icon="logout" sm negative />
-      </form>
+        <form method="POST" action="{{ route('logout') }}" class="flex space-x-2">
+          @csrf
+          <x-button @click="logout=false" label="Cancel" sm icon="x" />
+          <x-button href="{{ route('logout') }}"
+            onclick="event.preventDefault();
+          this.closest('form').submit();" label="Logout"
+            icon="logout" sm negative />
+        </form>
+
       </div>
     </div>
   </div>
@@ -120,23 +126,6 @@ aria-modal="true">
   @livewireScripts
 </body>
 <script>
-
-  //add javascript to handle the logout modal
-document.addEventListener('DOMContentLoaded', function () {
-  const logoutButton = document.querySelector('button[x-on\\:click="logout = true"]');
-  const logoutModal = document.getElementById('logoutModal');
-  const cancelLogoutBtn = document.getElementById('cancelLogoutBtn');
-
-  // Show the modal when the logout button is clicked
-  logoutButton.addEventListener('click', function () {
-    logoutModal.style.display = 'block';
-  });
-
-  // Hide the modal when the cancel button is clicked
-  cancelLogoutBtn.addEventListener('click', function () {
-    logoutModal.style.display = 'none';
-  });
-});
 
 
 function goFullscreen() {
