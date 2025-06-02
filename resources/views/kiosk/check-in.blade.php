12b8227dc1ecfc47df4cffcf419262060db8d372
Gabriel Jon Icawalo <67619600+GabJeungSook@users.noreply.github.com>
2025-06-02 13:43:02 +0800

    ss

--- resources/views/kiosk/check-in.blade.php
+++ resources/views/kiosk/check-in.blade.php
@@ -1,8 +1,8 @@
 <x-kiosk-layout-update>
   <div class="p-10">
     <div>
-     
-     <button id="print_qr" data-value="123" class="p-4 bg-green-500">Print QR</button>
+
+     {{-- <button id="print_qr" data-value="123" class="p-4 bg-green-500">Print QR</button> --}}
       {{-- <x-button id="print_qr" href="{{ route('kiosk.dashboard') }}" label="DONE" right-icon="check" positive lg class="font-bold" /> --}}
     </div>
     @livewire('kiosk.check-in')
