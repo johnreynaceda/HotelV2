dfb1c3bba7694bde225e9b0974a70c45adec1660
Gabriel Jon Icawalo <67619600+GabJeungSook@users.noreply.github.com>
2025-06-03 15:27:19 +0800

    api floors and rooms

---
+++ app/Http/Controllers/API/FloorController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Room;
+use App\Models\Floor;
+use Illuminate\Http\Request;
+use App\Models\TemporaryReserved;
+use App\Http\Controllers\Controller;
+use App\Models\TemporaryCheckInKiosk;
+
+class FloorController extends Controller
+{
+    public function indexWithRooms(Request $request, $branchId)
+    {
+        $temporaryCheckInKiosk = TemporaryCheckInKiosk::where(
+            'branch_id',
+            $branchId
+        )
+            ->pluck('room_id')
+            ->toArray();
+
+        $temporaryReserved = TemporaryReserved::where(
+            'branch_id',
+            $branchId
+        )
+            ->pluck('room_id')
+            ->toArray();
+        $typeId = $request->query('type_id');
+        $floorId = $request->query('floor_id'); // Optional single floor filter
+
+        $floors = Floor::with(['rooms' => function ($query) use ($typeId, $temporaryCheckInKiosk, $temporaryReserved, $floorId) {
+            $query->where('status', 'Available')
+                ->where('is_priority', true)
+                ->when($typeId, fn($q) => $q->where('type_id', $typeId))
+                ->when($floorId, fn($q) => $q->where('floor_id', $floorId))
+                ->whereNotIn('id', $temporaryCheckInKiosk)
+                ->whereNotIn('id', $temporaryReserved)
+                ->with(['type.rates'])
+                ->orderBy('number', 'asc')
+                ->take(10);
+        }])
+        ->where('branch_id', $branchId)
+        ->orderBy('number')
+        ->get();
+
+        return response()->json([
+            'success' => true,
+            'data' => $floors,
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\TypeController;
+use App\Http\Controllers\Api\FloorController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,4 +23,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->get('/room-types', [TypeController::class, 'index']);
+Route::get('/branch/{branch}/floors-with-rooms', [FloorController::class, 'indexWithRooms']);
 
