63f4b8a7d893dc35168f84499a04e7d43bca1956
Gabriel Jon Icawalo <67619600+GabJeungSook@users.noreply.github.com>
2025-06-09 13:22:24 +0800

    aa

--- app/Http/Controllers/API/FloorController.php
+++ app/Http/Controllers/API/FloorController.php
@@ -16,9 +16,62 @@ class FloorController extends Controller
     public function index(Request $request, $branchId)
     {
         try{
+            $temporaryCheckInKiosk = TemporaryCheckInKiosk::where(
+            'branch_id',
+            $branchId
+            )
+            ->get()
+            ->pluck('room_id')
+            ->toArray();
+
+
+
+            $temporaryReserved = TemporaryReserved::where(
+            'branch_id',
+            $branchId
+            )
+            ->pluck('room_id')
+            ->toArray();
+
+            $typeId = $request->query('type_id');
+
+            $floorId = $request->query('floor_id'); // Optional single floor filter
+
+            $floors = Floor::with(['rooms' => function ($query) use (
+            $typeId,
+            $temporaryCheckInKiosk,
+            $temporaryReserved,
+            $floorId
+        ) {
+            $query->where('status', 'Available')
+                ->where('is_priority', true)
+                ->when($typeId, fn($q) => $q->where('type_id', $typeId))
+                ->when($floorId, fn($q) => $q->where('floor_id', $floorId))
+                ->whereNotIn('id', $temporaryCheckInKiosk)
+                ->whereNotIn('id', $temporaryReserved)
+                ->with(['type.rates'])
+                ->orderBy('number', 'asc');
+        }])
+        ->where('branch_id', $branchId)
+        ->orderBy('number')
+        ->get();
+
+
+
+
+
+        // Then manually limit rooms in each floor (if needed)
+        foreach ($floors as $floor) {
+            $floor->setRelation('rooms', $floor->rooms->take(10));
+        }
+
+        //  return response()->json([
+        //         'success' => true,
+        //         'data' => $floors,
+        //     ]);
             return ApiResponse::success([
                 $request,
-                'branchId' => $branchId,
+                'data' => $floors,
             ], 200);
         }catch (\Exception $e) {
             Log::error('API Error: '.$e->getMessage(), [
