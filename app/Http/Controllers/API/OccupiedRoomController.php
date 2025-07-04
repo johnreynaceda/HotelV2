fcdb8c44f727818d45daa5256d9e90261adba4d2
Gabriel Jon Icawalo <67619600+GabJeungSook@users.noreply.github.com>
2025-07-04 10:35:09 +0800

    test

--- app/Http/Controllers/API/OccupiedRoomController.php
+++ app/Http/Controllers/API/OccupiedRoomController.php
@@ -17,7 +17,7 @@ class OccupiedRoomController extends Controller
     {
         try {
             $floors = Floor::with(['rooms' => function ($query) {
-                    $query->where('status', 'Occupied')->with(['checkInDetail.guest']);
+                    $query->where('status', 'Occupied')->with(['checkInDetail.guest.type']);
                 }])
                 ->orderBy('number')
                 ->get();
