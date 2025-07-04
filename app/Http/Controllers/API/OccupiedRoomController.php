0302a592e3c28c4f83d17d7f466ecb370ef5485a
Gabriel Jon Icawalo <67619600+GabJeungSook@users.noreply.github.com>
2025-07-04 11:18:47 +0800

    ss

--- app/Http/Controllers/API/OccupiedRoomController.php
+++ app/Http/Controllers/API/OccupiedRoomController.php
@@ -13,14 +13,41 @@ use Illuminate\Support\Facades\Log;
 
 class OccupiedRoomController extends Controller
 {
-    public function occupiedRooms()
+    // public function occupiedRooms()
+    // {
+    //     try {
+    //         $floors = Floor::with(['rooms' => function ($query) {
+    //                 $query->where('status', 'Occupied')->with(['checkInDetail.guest.type']);
+    //             }])
+    //             ->orderBy('number')
+    //             ->get();
+
+    //         return ApiResponse::success(['data' => $floors], 200);
+    //     } catch (\Exception $e) {
+    //         Log::error('Occupied Rooms API Error: ' . $e->getMessage(), [
+    //             'trace' => $e->getTrace()
+    //         ]);
+    //         return ApiResponse::error($e->getMessage());
+    //     }
+    // }
+
+    public function occupiedRooms(Request $request)
     {
         try {
-            $floors = Floor::with(['rooms' => function ($query) {
-                    $query->where('status', 'Occupied')->with(['checkInDetail.guest.type']);
-                }])
-                ->orderBy('number')
-                ->get();
+            $qrCode = $request->query('qr_code'); // Get the qr_code query param
+
+            $floors = Floor::with(['rooms' => function ($query) use ($qrCode) {
+                $query->where('status', 'Occupied')
+                    ->with(['checkInDetail.guest.type']);
+
+                if ($qrCode) {
+                    $query->whereHas('checkInDetail.guest', function ($guestQuery) use ($qrCode) {
+                        $guestQuery->where('qr_code', $qrCode);
+                    });
+                }
+            }])
+            ->orderBy('number')
+            ->get();
 
             return ApiResponse::success(['data' => $floors], 200);
         } catch (\Exception $e) {
