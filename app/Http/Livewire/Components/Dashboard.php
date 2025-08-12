292e8b959c99f4fdea27a431a09a6bdb09130f2a
Gabriel Jon Icawalo <67619600+GabJeungSook@users.noreply.github.com>
2025-08-12 08:26:26 +0800

    todays transaction

--- app/Http/Livewire/Components/Dashboard.php
+++ app/Http/Livewire/Components/Dashboard.php
@@ -23,10 +23,20 @@ class Dashboard extends Component
         $this->check_in_today = CheckinDetail::whereDate('check_in_at', Carbon::today())->count();
         $this->check_out_today = CheckinDetail::where('is_check_out', 1)->whereDate('check_out_at', Carbon::today())->count();
         $this->expected_check_out = CheckinDetail::where('is_check_out', 0)->whereDate('check_out_at', Carbon::today())->count();
-        $this->total_check_in = CheckinDetail::where('is_check_out', 0)->count();
-        $this->total_check_out = CheckinDetail::where('is_check_out', 1)->count();
-        $this->total_available_rooms = Room::where('branch_id', auth()->user()->branch_id)->where('status', 'Available')->count();
-        $this->total_cleaning_rooms = Room::where('branch_id', auth()->user()->branch_id)->where('status', 'Cleaning')->count();
+        $this->total_check_in = CheckinDetail::where('is_check_out', 0)
+            ->whereDate('check_in_at', Carbon::today())
+            ->count();
+        $this->total_check_out = CheckinDetail::where('is_check_out', 1)
+            ->whereDate('check_out_at', Carbon::today())
+            ->count();
+        $this->total_available_rooms = Room::where('branch_id', auth()->user()->branch_id)
+            ->where('status', 'Available')
+            ->whereDate('updated_at', Carbon::today())
+            ->count();
+        $this->total_cleaning_rooms = Room::where('branch_id', auth()->user()->branch_id)
+            ->where('status', 'Cleaning')
+            ->whereDate('updated_at', Carbon::today())
+            ->count();
     }
 
 
